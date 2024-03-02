5ce740455e82465523782c9163684a03de37cac6
Daniil <danya.pavlinov01@mail.ru>
2024-03-02 16:38:40 +0300

    Removed the crutches from the code: now the positions and sizes of objects depend on the resolution of the window, added the Object parent class, the Scene class displaying the game scenes

--- main.py
+++ main.py
@@ -3,49 +3,7 @@ from raylib import colors
 import random
 
 
-class Player:
-    def __init__(self, pos_x, pos_y, width, height, color, shift, name, count_of_wins):
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        self.width = width
-        self.height = height
-        self.color = color
-        self.shift = shift
-        self.name = name
-        self.count_of_wins = count_of_wins
-
-    def rendering_and_movement(self, window_height):
-        self.draw_rect()
-        self.pressed_key(window_height)
-
-    def draw_rect(self):
-        pyray.draw_rectangle(self.pos_x, self.pos_y, self.width, self.height, self.color)
-
-    def pressed_key(self, window_height):
-        if self.name == "Rl1: ":
-            if pyray.is_key_down(pyray.KeyboardKey(87)):
-                if self.pos_y >= 0:
-                    self.pos_y -= self.shift
-            if pyray.is_key_down(pyray.KeyboardKey(83)):
-                if self.pos_y + self.height <= window_height:
-                    self.pos_y += self.shift
-        else:
-            if pyray.is_key_down(pyray.KeyboardKey(87)):
-                if self.pos_y >= 0:
-                    self.pos_y -= self.shift
-            if pyray.is_key_down(pyray.KeyboardKey(83)):
-                if self.pos_y + self.height <= window_height:
-                    self.pos_y += self.shift
-
-    def draw_text(self, window_width):
-        collision_text_format = self.name + str(self.count_of_wins)
-        if self.name == "Pl1: ":
-            pyray.draw_text(collision_text_format.format(), 10, 10, 50, colors.BLUE)
-        else:
-            pyray.draw_text(collision_text_format.format(), window_width - 145, 10, 50, colors.RED)
-
-
-class Ball:
+class Object:
     def __init__(self, pos_x, pos_y, width, height, color, shift_x, shift_y):
         self.pos_x = pos_x
         self.pos_y = pos_y
@@ -55,10 +13,58 @@ class Ball:
         self.shift_x = shift_x
         self.shift_y = shift_y
 
-    def draw_ball(self):
+    def reset(self, pos_x, pos_y, shift_x, shift_y):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.shift_x = shift_x
+        self.shift_y = shift_y
+
+    def draw_object(self):
         pyray.draw_rectangle(self.pos_x, self.pos_y, self.width, self.height, self.color)
 
-    def shift(self):
+
+class Player(Object):
+    def __init__(self, pos_x, pos_y, width, height, color, shift_x, shift_y, name, count_of_wins):
+        super().__init__(pos_x, pos_y, width, height, color, shift_x, shift_y)
+        self.name = name
+        self.count_of_wins = count_of_wins
+
+    def rendering_and_movement(self, window_height):
+        self.draw_object()
+        self.pressed_key(window_height)
+
+    def pressed_key(self, window_height):
+        if self.name == "Pl1: ":
+            if pyray.is_key_down(pyray.KeyboardKey(87)):
+                if self.pos_y >= 0:
+                    self.pos_y -= self.shift_y
+            if pyray.is_key_down(pyray.KeyboardKey(83)):
+                if self.pos_y + self.height <= window_height:
+                    self.pos_y += self.shift_y
+        else:
+            if pyray.is_key_down(pyray.KeyboardKey(73)):
+                if self.pos_y >= 0:
+                    self.pos_y -= self.shift_y
+            if pyray.is_key_down(pyray.KeyboardKey(75)):
+                if self.pos_y + self.height <= window_height:
+                    self.pos_y += self.shift_y
+
+    def draw_text(self, window_width):
+        collision_text_format = self.name + str(self.count_of_wins)
+        if self.name == "Pl1: ":
+            pyray.draw_text(collision_text_format.format(), window_width // 2 - 50 - 100, self.pos_y // 6, 50, colors.BLUE)
+        else:
+            pyray.draw_text(collision_text_format.format(), window_width // 2 - 50 + 100, self.pos_y // 6, 50, colors.RED)
+
+    def winner(self):
+        self.count_of_wins += 1
+
+
+class Ball(Object):
+    def __init__(self, pos_x, pos_y, width, height, color, shift_x, shift_y):
+        super().__init__(pos_x, pos_y, width, height, color, shift_x, shift_y)
+
+    def ball_shift(self):
         self.pos_x += self.shift_x
         self.pos_y += self.shift_y
 
@@ -66,35 +72,34 @@ class Ball:
         self.shift_x *= -1
 
     def collide_with_vertical_border(self, window_height):
-        if self.pos_y <= 0 or self.pos_y >= window_height - self.width:
+        if self.pos_y <= 0 or self.pos_y >= window_height - self.height:
             return True
         return False
 
     def collide_with_horizontal_border(self, window_width):
-        if self.pos_x <= 0 or self.pos_x + self.height >= window_width:
+        if self.pos_x <= 0 or self.pos_x + self.width >= window_width:
             return True
 
-    def collide_with_plrs(self, plr1_pos_x, plr1_pos_y, plr2_pos_x, plr2_pos_y):
-        if collide_with_plr1(plr1_pos_x, plr1_pos_y, self.pos_x, self.pos_y):
+    def collide_with_plrs(self, plr1_pos_x, plr1_pos_y, plr2_pos_x, plr2_pos_y, plr_width, plr_height):
+        if collide_with_plr1(plr1_pos_x, plr1_pos_y, self.pos_x, self.pos_y, plr_width, plr_height, self.height):
             return True
-        if collide_with_plr2(plr2_pos_x, plr2_pos_y, self.pos_x, self.pos_y):
+        if collide_with_plr2(plr2_pos_x, plr2_pos_y, self.pos_x, self.pos_y, plr_height, self.width, self.height):
             return True
         return False
 
-    def check_collides(self, window_width, window_height, plr1_pos_x, plr1_pos_y, plr2_pos_x, plr2_pos_y):
+    def check_collides(self, window_width, window_height, plr1_pos_x, plr1_pos_y, plr2_pos_x, plr2_pos_y, plr_width, plr_height):
         # проверка на коллизию мяча с вертикалями
         if self.collide_with_vertical_border(window_height):
             self.shift_y *= -1
             return False
         # проверка на коллизию мяча с игроками
-        if self.collide_with_plrs(plr1_pos_x, plr1_pos_y, plr2_pos_x, plr2_pos_y):
+        if self.collide_with_plrs(plr1_pos_x, plr1_pos_y, plr2_pos_x, plr2_pos_y, plr_width, plr_height):
             self.bounce()
             if self.shift_x > 0:
                 self.shift_x += 1
-                self.shift_y += random.randint(-5, 5)
             else:
                 self.shift_x -= 1
-                self.shift_y += random.randint(-5, 5)
+            self.shift_y += random.choice([-window_height // 225 * 2, window_height // 225 * 2])
             return False
         # конец игры
         if self.collide_with_horizontal_border(window_width):
@@ -102,25 +107,76 @@ class Ball:
 
     def who_win(self, window_width):
         if self.pos_x <= 0:
-            return "red"
-        elif self.pos_x + 20 >= window_width:
-            return "blue"
+            return "Pl2"
+        elif self.pos_x + self.width >= window_width:
+            return "Pl1"
 
 
-def collide_with_plr1(plr_pos_x, plr_pos_y, ball_pos_x, ball_pos_y):
-    if plr_pos_x + 20 >= ball_pos_x and (plr_pos_y <= ball_pos_y + 20 and plr_pos_y + 135 >= ball_pos_y):
+class Scene:
+    def __init__(self, scene):
+        self.scene = scene
+
+    def scene_changed(self, window_width, window_height, player1, player2, ball):
+        if self.scene == "menu":
+            self.reset_objects(window_width, window_height, player1, player2, ball)
+            self.scene_menu(window_width, window_height, player1, player2)
+        elif self.scene == "game":
+            self.scene_game(window_width, window_height, player1, player2, ball)
+        elif self.scene == "exit":
+            self.exit()
+
+    def reset_objects(self, window_width, window_height, player1, player2, ball):
+        player1.reset(window_height // 30, window_height // 2 - window_height // 6, 0, window_height // 225,)
+        player2.reset(window_width - window_height // 30 * 2, window_height // 2 - window_height // 6, 0, window_height // 225,)
+        ball.reset(window_width // 2 - window_height // 30, window_height // 2 - window_height // 30,
+            random.choice([-window_height // 225 * 2, window_height // 225 * 2]), random.choice([-window_height // 225, window_height // 225]))
+
+    def scene_menu(self, window_width, window_height, player1, player2):
+        #кнопки
+        button_scene_new_game = pyray.Rectangle(window_width / 2 - 100 / 2, window_height / 2 - 10 - 50, 100, 50)
+        button_scene_exit = pyray.Rectangle(window_width / 2 - 100 / 2, window_height / 2 + 10, 100, 50)
+
+        #отрисовка менюшки
+        player1.draw_text(window_width)
+        player2.draw_text(window_width)
+        if pyray.gui_button(button_scene_new_game, 'New game'):
+            self.scene = "game"
+        if pyray.gui_button(button_scene_exit, 'Exit'):
+            self.scene = "exit"
+
+    def scene_game(self, window_width, window_height, player1, player2, ball):
+        # отрисовкац
+        player1.rendering_and_movement(window_height)
+        player2.rendering_and_movement(window_height)
+        ball.draw_object()
+
+        # движение мяча
+        ball.ball_shift()
+
+        # проверка на коллизию
+        if ball.check_collides(window_width, window_height, player1.pos_x, player1.pos_y, player2.pos_x, player2.pos_y,
+                               window_height // 22, window_height // 3):
+            self.scene = "menu"
+            if ball.who_win(window_width) == "Pl1":
+                player1.winner()
+            else:
+                player2.winner()
+
+    def exit(self):
+        pyray.close_window()
+        exit(0)
+
+
+def collide_with_plr1(plr_pos_x, plr_pos_y, ball_pos_x, ball_pos_y, plr_width, plr_height, ball_height):
+    if plr_pos_x + plr_width >= ball_pos_x and (plr_pos_y <= ball_pos_y + ball_height and plr_pos_y + plr_height >= ball_pos_y):
         return True
     return False
 
 
-def collide_with_plr2(plr_pos_x, plr_pos_y, ball_pos_x, ball_pos_y):
-    if plr_pos_x <= ball_pos_x + 20 and (plr_pos_y <= ball_pos_y + 20 and plr_pos_y + 135 >= ball_pos_y):
+def collide_with_plr2(plr_pos_x, plr_pos_y, ball_pos_x, ball_pos_y, plr_height, ball_width, ball_height):
+    if plr_pos_x <= ball_pos_x + ball_width and (plr_pos_y <= ball_pos_y + ball_height and plr_pos_y + plr_height >= ball_pos_y):
         return True
     return False
-
-
-def scene_changed(name):
-    return name
 
 
 def end_game():
@@ -130,56 +186,26 @@ def end_game():
 
 def main():
     #окно
-    window_width, window_height = 1050, 450
+    window_width, window_height = 1920, 1080
     pyray.init_window(window_width, window_height, 'PingPong')
     pyray.set_target_fps(60)
 
-    #кнопки
-    button_scene_new_game = pyray.Rectangle(window_width / 2 - 100 / 2, window_height / 2 - 10 - 50, 100, 50)
-    button_scene_exit = pyray.Rectangle(window_width / 2 - 100 / 2, window_height / 2 + 10, 100, 50)
-
-    plr_win = ""
-    scene = "menu"
-    player1_count_of_wins = 0
-    player2_count_of_wins = 0
+    # инициальизация объектов
+    player1 = Player(window_height // 30, window_height // 2 - window_height // 6, window_height // 30, window_height // 3, colors.BLUE, 0, window_height // 225,
+                     "Pl1: ", 0)
+    player2 = Player(window_width - window_height // 30 * 2, window_height // 2 - window_height // 6, window_height // 30, window_height // 3, colors.RED, 0, window_height // 225,
+                     "Pl2: ", 0)
+    ball = Ball(window_width // 2 - window_height // 30, window_height // 2 - window_height // 30, window_height // 30,
+                window_height // 30, colors.WHITE, random.choice([-window_height // 225 * 2, window_height // 225 * 2]), random.choice([-window_height // 225, window_height // 225]))
+    scene = Scene("menu")
 
     while not pyray.window_should_close():
         pyray.begin_drawing()
-        if scene == "menu":
-            #позиции для игроков и мячей
-            player1 = Player(20, window_height // 2 - 70, 20, 140, colors.BLUE, 2, "Pl1: ", player1_count_of_wins)
-            player2 = Player(window_width - 40, window_height // 2 - 70, 20, 140, colors.RED, 2, "Pl2: ", player2_count_of_wins)
-            ball = Ball(window_width // 2 - 20, window_height // 2 - 10, 20, 20, colors.WHITE, random.choice([-4, 4]), random.choice([-2, 2]))
-
-            player1.draw_text(window_width)
-            player2.draw_text(window_width)
-            if pyray.gui_button(button_scene_new_game, 'New game'):
-                scene = scene_changed("game")
-            if pyray.gui_button(button_scene_exit, 'Exit'):
-                end_game()
-
-        elif scene == "game":
-            #отрисовка
-            player1.rendering_and_movement(window_height)
-            player2.rendering_and_movement(window_height)
-            ball.draw_ball()
-
-            #движение мяча
-            ball.shift()
-
-            # проверка на коллизию
-            if ball.check_collides(window_width, window_height, player1.pos_x, player1.pos_y, player2.pos_x, player2.pos_y):
-                scene = scene_changed("menu")
-                plr_win = ball.who_win(window_width)
-                if plr_win == "blue":
-                    player1_count_of_wins += 1
-                else:
-                    player2_count_of_wins += 1
-
+        scene.scene_changed(window_width, window_height, player1, player2, ball)
         pyray.clear_background(colors.BLACK)
         pyray.end_drawing()
     pyray.close_window()
 
 
 if __name__ == '__main__':
-        main()
+    main()
